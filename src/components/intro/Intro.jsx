74777839bb053bc6f3bafb5994c3d7f338f261e1
Toye Sobayo <sobayotoye482@gmail.com>
2023-11-11 23:05:13 -0500

    added animation for intro titles

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,15 +7,16 @@ const Intro = () => {
     <div className ="i">
       <div className = "i-left">
         <div className = "i-left-wrapper">
-          <h2 className = "i-intro">Yo</h2>
-          <h1 className = "i-name">Toye Sobayo</h1>
-          <div className = "i-title"> 
-           <div className = "i-title-wrapper">
+         <h2 className = "i-intro">Yo, I'm</h2>
+         <h2 className = "i-name">Toye Sobayo</h2>
+         <div className = "i-title"> 
+          <div className = "i-title-wrapper">
            <div className = "i-title-item">Student</div> 
            <div className = "i-title-item">Programmer</div>
-           <div className = "i-title-item">The Honored One</div>
-           </div>
+           <div className = "i-title-item">MongoDB Glazer</div>
+           <div className = "i-title-item">LeetCode Warrior</div>
           </div>
+         </div>
         </div>
       </div>
       <div className = "i-right">right</div>
